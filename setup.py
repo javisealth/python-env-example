73eb9a64c7f5c724ecab8b69b19fc20cf56864a6
Rodrigo Núñez Mujica <rodrigo.nunez@innovation4security.com>
2016-03-12 12:40:20 +0100

    Added installable setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from src import killer_app
+from setuptools import setup, find_packages
+
+# Package dependencies
+install_requires = [
+
+]
+
+# Trove classifiers
+classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3.5',
+]
+
+
+setup(
+    # Package info
+    name=killer_app.name,
+    version=killer_app.version,
+    description=killer_app.description,
+    author=killer_app.author,
+    author_email=killer_app.author_email,
+    url='',
+    long_description=killer_app.description,
+
+    # Package classifiers
+    classifiers=classifiers,
+
+    # Package structure
+    packages=find_packages('src', exclude=['ez_setup', '*.tests', '*.tests.*', 'tests.*', 'tests']),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+
+    # Dependencies
+    install_requires=install_requires,
+
+)
---
+++ src/killer_app/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+name = 'Killer App!'
+version = '0.0.0'
+description = 'Example Killer App'
+author = 'Rodrigo Núñez Mujica'
+author_email = 'rnunezmujica@icloud.com'
